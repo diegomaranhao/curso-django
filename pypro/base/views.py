ba7d6cf5d44789698dec38b2c9b77fdd6711b9ee
Diego Maranhão <diegomaranhao@outlook.com>
2020-05-29 08:42:58 -0400

    Instalado o Sentry close #40

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+    raise ValueError()
     return HttpResponse('<html><body>Olá</body></html>', content_type='text/html')
