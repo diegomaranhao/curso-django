dbd5fdc32bec18e67a3c4489a8a4a0157a4516d0
Diego Maranhão <diegomaranhao@outlook.com>
2020-05-29 08:23:59 -0400

    Instalado o Django Debug toolbar close #38

--- pypro/settings.py
+++ pypro/settings.py
@@ -76,6 +76,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pypro.wsgi.application'
 
+INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+# Conf django debug toolbar
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware', )
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
