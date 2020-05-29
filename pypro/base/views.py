6555a1cbb8be3101356ca62ae39f2c7ead790ac2
Diego Maranhão <diegomaranhao@outlook.com>
2020-05-29 08:23:59 -0400

    Instalado o Django Debug toolbar close #38

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Olá')
+    return HttpResponse('<html><body>Olá</body></html>', content_type='text/html')
