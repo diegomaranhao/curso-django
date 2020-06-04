3aeb46dca4a284a91589bbe5b67c92e40b4e4f54
Diego Maranhão <diegomaranhao@outlook.com>
2020-06-04 15:30:47 -0400

    Incorporado primeiro video de forma manual close #70

---
+++ pypro/aperitivos/tests.py
@@ -0,0 +1,22 @@
+import pytest
+# Create your views here.
+from django.urls import reverse
+
+from pypro.base.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+
+
+def teste_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_video_title(resp):
+    assert_contains(resp, '<h2>Motivação</h2>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/425991579"')
