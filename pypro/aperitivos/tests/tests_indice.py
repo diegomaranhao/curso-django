fcb1df693896e747d024ae8037dc4f88f05fd34a
Diego Maranhão <diegomaranhao@outlook.com>
2020-06-05 12:44:08 -0400

    Criado a lista de videos aperitivos close #76

---
+++ pypro/aperitivos/tests/tests_indice.py
@@ -0,0 +1,29 @@
+import pytest
+# Create your views here.
+from django.urls import reverse
+
+from pypro.base.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def teste_status_code(resp):
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Motivação',
+        'Passar do dia'
+    ]
+)
+def test_video_title(resp, titulo):
+    assert_contains(resp, titulo)
+#
+#
+# def test_conteudo_video(resp):
+#     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/425991579"')
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'vimeo_id': 425991579, 'titulo': 'Motivação'},
+    {'slug': 'passar-do-dia', 'vimeo_id': 426262500, 'titulo': 'Passar do dia'}
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'slug': 'Motivacao', 'vimeo_id': 425991579, 'titulo': 'Motivação'},
-        'passar-do-dia': {'vimeo_id': 426262500, 'titulo': 'Passar do dia'}
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
