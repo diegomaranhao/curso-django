6f7ad64f52b44455a44d4a562609711a2e66872a
Diego Maranhão <diegomaranhao@outlook.com>
2020-06-16 15:14:52 -0400

    Criação da página de detalhe das aulas close #129

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 def listar_modulos_ordenados() -> List[Modulo]:
@@ -17,3 +17,7 @@ def encontrar_modulo(slug: str) -> Modulo:
 
 def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
+
+
+def econtrar_aula(slug):
+    return Aula.objects.get(slug=slug)
