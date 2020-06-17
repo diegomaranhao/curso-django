704d0fa869c259162dc3333bd5bbbf819881a57b
Diego Maranhão <diegomaranhao@outlook.com>
2020-06-17 14:57:28 -0400

    Criado índice de módulos contendo todas as aulas dos respectivos módulos close #139

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from django.db.models import Prefetch
+
 from pypro.modulos.models import Modulo, Aula
 
 
@@ -21,3 +23,9 @@ def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
 
 def econtrar_aula(slug):
     return Aula.objects.select_related('modulo').get(slug=slug)
+
+
+def listar_modulos_com_aulas():
+    aulas_ordenadas = Aula.objects.order_by('order')
+    return Modulo.objects.order_by('order').prefetch_related(
+        Prefetch('aula_set', queryset=aulas_ordenadas, to_attr='aulas')).all()
--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -13,3 +13,8 @@ def detalhe(request, slug):
 def aula(request, slug):
     aula = facade.econtrar_aula(slug)
     return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
+
+
+def indice(request):
+    ctx = {'modulos': facade.listar_modulos_com_aulas()}
+    return render(request, 'modulos/modulos_indice.html', context=ctx)
