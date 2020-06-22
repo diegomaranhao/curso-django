cce1f411cce5fb2d1d0a29c29e33cc08d5dbc6c0
Diego Maranhão <diegomaranhao@outlook.com>
2020-06-22 14:49:13 -0400

    Adicionado Controle de acesso para os usuários close #154

---
+++ pypro/base/tests/test_login.py
@@ -0,0 +1,32 @@
+import pytest
+from django.urls import reverse
+from model_mommy import mommy
+
+
+@pytest.fixture
+def resp(client, db):
+    return client.get(reverse('login'))
+
+
+def test_login_form_page(resp):
+    assert resp.status_code == 200
+
+
+@pytest.fixture
+def usuario(db, django_user_model):
+    usuario_modelo = mommy.make(django_user_model)
+    senha = 'senha'
+    usuario_modelo.set_password(senha)
+    usuario_modelo.save()
+    usuario_modelo.senha_plana = senha
+    return usuario_modelo
+
+
+@pytest.fixture
+def resp_post(client, usuario):
+    return client.post(reverse('login'), {'username': usuario.email, 'password': usuario.senha_plana})
+
+
+def test_login_redirect(resp_post):
+    assert resp_post.status_code == 302
+    assert resp_post.url == reverse('modulos:indice')
