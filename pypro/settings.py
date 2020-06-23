fc57deb8880de101c21913824597d3609334600f
Diego Maranhão <diegomaranhao@outlook.com>
2020-06-23 14:28:25 -0400

    Implementado o Logout do usuário close #162

--- pypro/settings.py
+++ pypro/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 AUTH_USER_MODEL = 'base.User'
 
 LOGIN_REDIRECT_URL = '/modulos/'
+LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
